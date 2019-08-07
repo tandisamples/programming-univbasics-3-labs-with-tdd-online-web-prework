6129d721667dbaac1ecdc1695ea5b202740bf5be
Tandi Samples <tandisamples@gmail.com>
2019-08-07 18:09:26 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,21 @@
 # Add your variables here
+first_number = 7
+second_number = 14
+
+sum = first_number + second_number
+
+puts sum
+
+expect(second_number).not_to equal(0)
+
+sum = first_number - second_number
+
+puts sum
+
+sum = first_number * second_number
+
+puts sum
+
+sum = first_number / second_number
+
+puts sum
